e71daeb9a3b0b0769f1887c0db6ed4f6832bdd5e
bishuuu <bisratzewdie@gmail.com>
2021-02-11 12:21:23 +0000

    print numbers using _putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: Void
+ */
+void print_numbers(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n')
+}
