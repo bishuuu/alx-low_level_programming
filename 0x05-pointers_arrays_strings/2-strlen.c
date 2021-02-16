d18d359954d1ee440365815bfa81bd2ee3de745f
bishuuu <bisratzewdie@gmail.com>
2021-02-16 08:49:20 +0000

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen - string length
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+return (length);
+}
